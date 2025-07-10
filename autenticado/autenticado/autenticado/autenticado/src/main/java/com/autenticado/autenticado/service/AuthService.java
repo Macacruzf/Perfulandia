6684fd4bf46383f7834034988f37d7ebe238b786
Macacruzf <maca.cruz@duocuc.cl>
2025-07-09 23:51:45 -0400

    Delete autenticado/autenticado/autenticado/autenticado directory

--- autenticado/autenticado/autenticado/autenticado/src/main/java/com/autenticado/autenticado/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.autenticado.autenticado.service;
-
-import com.autenticado.autenticado.model.Usuario;
-
-public interface AuthService {
-    Usuario autenticar(String nickname, String password);
-    Usuario buscarPorNickname(String nickname);
-}
